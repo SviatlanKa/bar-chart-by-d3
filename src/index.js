7ee31d2d233d48b192a130441013911fe09759b2
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-09-24 15:46:09 +0300

    finished project

--- src/index.js
+++ src/index.js
@@ -12,10 +12,25 @@ const paddingBottom = 50;
 const paddingVert = paddingTop + paddingBottom;
 const paddingHoriz = paddingLeft + paddingRight;
 
+const tooltip = d3.select("#bar-chart")
+                .append("div")
+                .attr("id", "tooltip")
+                .style("opacity", 0);
+
 const barChart = d3.select('#bar-chart')
                 .append("svg")
                 .attr("height", svgHeight)
                 .attr("width", svgWidth);
+
+const defs = barChart.append("defs");
+
+const filter = defs.append("filter")
+    .attr("id", "shadow")
+    .append("feDropShadow")
+    .attr("dx", -0.2)
+    .attr("dy", -0.2)
+    .attr("stdDeviation", 2.5);
+
 
 const chartPlot = barChart.append("g")
                         .attr("transform", `translate(${paddingLeft},${paddingTop})`);
@@ -23,11 +38,17 @@ const chartPlot = barChart.append("g")
 d3.json(url).then(data => {
     let dataset = [];
 
-    data.data.forEach(el => dataset.push(el));
+    data.data.forEach(el => {
+        const item = {
+            date: new Date(el[0]),
+            value: el[1]
+        };
+        dataset.push(item);
+    });
 
-    const maxVal = d3.max(dataset, d => d[1]);
-    const scaleX = d3.scaleBand()
-                    .domain(dataset.map(d => d[0]))
+    const maxVal = d3.max(dataset, d => d.value);
+    const scaleX = d3.scaleTime()
+                    .domain([d3.min(dataset, d => d.date), d3.max(dataset, d => d.date)])
                     .range([0, svgWidth - paddingHoriz]);
     const scaleY = d3.scaleLinear()
                     .domain([0, maxVal])
@@ -40,37 +61,48 @@ d3.json(url).then(data => {
     const xAxisTranslate = svgHeight - paddingVert;
     chartPlot.append("g")
         .attr("id", "x-axis")
+        .style("font-size", ".8em")
         .attr("transform", `translate(0,${xAxisTranslate})`)
-        .call(xAxis);
+        .call(xAxis.ticks(d3.timeYear.every(5)));
 
     chartPlot.append("g")
         .attr("id", "y-axis")
+        .style("font-size", ".8em")
         .call(yAxis);
-
-
 
     chartPlot.selectAll("rect")
         .data(dataset)
         .enter()
         .append("rect")
         .attr("class", "bar")
-        .attr("x", d => scaleX(d[0]))
-        .attr("y", d => scaleY(d[1]))
-        .style("width", scaleX.bandwidth())
-        .style("height", d => svgHeight - paddingVert - scaleY(d[1]))
-    //     .append("div")
-    //     .attr("class", "tip");
-    //     .html(d => d[0] + " <i>" + d[1]
+        .property("data-date", d => d.date)
+        .property("data-gdp", d => d.value)
+        .attr("x", d => scaleX(d.date))
+        .attr("y", d => scaleY(d.value))
+        .attr("width", (svgWidth - paddingHoriz) / dataset.length)
+        .attr("height", d => svgHeight - paddingVert - scaleY(d.value))
+        .style("filter", "url(#shadow)")
+        .on("mouseover", d => {
+            tooltip.transition()
+                    .duration(350)
+                    .style("opacity", .6);
+            tooltip.html("<span>" + d3.timeFormat("%B %Y")(d.date) + "<br>"
+                + d3.format("$,")(d.value) + " Billion" + "</span>")
+                    .style("left", d3.event.pageX + 15 + "px")
+                    .style("top", d3.event.pageY - 75 + "px");
+        })
+        .on("mouseout", (d) => {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0);
+        });
 
-
-    //     .append("text")
-    //         .attr("text-anchor", "end")
-    //         .attr("transform", "rotate(-90)")
-    //         .attr("x", 10)
-    //         .attr("y", -20)
-    //         .text("GDP, billions of dollars");+"</i>")
-
-
+    chartPlot.append("text")
+            .attr("text-anchor", "end")
+            .attr("transform", "rotate(-90)")
+            .attr("x", -50)
+            .attr("y", paddingLeft / 2)
+            .text("GDP, in billions USD");
 });
 
 
